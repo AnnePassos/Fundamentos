50afb249b7fc5699469814197f84010e4442cdf3
AnnePassos <annecpassos@outlook.com>
2024-08-29 21:27:55 -0300

    Aula 1

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello, Mundo!");  
+alert("Hello, Word");
+//let preco = prompt("Digite o preço");
+//document.write(preco);
+
+let nome = prompt("Digite o seu nome");
+document.write("Óla " ,nome, ". Seja bem-vindx ao site");
+//óla, fulana. Seja bem-vindx ao site!
